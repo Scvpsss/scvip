ee20bc936e91c8d2dbd2e0d7e7455e31699574c6
Scvpsss <158008928+Scvpsss@users.noreply.github.com>
2024-02-02 19:49:10 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -279,7 +279,7 @@ echo -e "   ${z}┗━━━━━━━━━━━━━━━━━━━━�
 echo -e "  ${IWhite} SSH ${NC}: $ressh"" ${IWhite} NGINX ${NC}: $resngx"" ${IWhite}  XRAY ${NC}: $resv2r"" ${IWhite} TROJAN ${NC}: $resv2r"
 echo -e "${IWhite}     STUNNEL ${NC}: $resst" "${IWhite} DROPBEAR ${NC}: $resdbr" "${IWhite} SSH-WS ${NC}: ${green}ON"
 echo -e "  ${z}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\033[0m${NC}"
-echo -e "  ${z}│ [${r}01${NC}] ${z}SSH    MENU  ${NC}       ""[${r}07${NC}] ${z}STATUS RUNNING     ${NC}"
+echo -e "  ${z}│ [${r}01${NC}] ${z}SSH    MENU  ${NC}       ""[${r}07${NC}] ${z}STATUS RUNNING     ${NC}"${z}│
 echo -e "  ${z}│ [${r}02${NC}] ${z}VMESS  MENU  ${NC}       ""[${r}08${NC}] ${z}CHANGE BANNER      ${NC}"
 echo -e "  ${z}│ [${r}03${NC}] ${z}VLESS  MENU  ${NC}       ""[${r}09${NC}] ${z}BACKUP & RESTORE   ${NC}"
 echo -e "  ${z}│ [${r}04${NC}] ${z}TROJAN MENU  ${NC}       ""[${r}10${NC}] ${z}INSTALL UDP        ${NC}"
