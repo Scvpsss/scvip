ec9b9916492973ef98aa3aa26f287aabc1cbf6df
Scvpsss <158008928+Scvpsss@users.noreply.github.com>
2024-02-02 19:46:43 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -262,7 +262,7 @@ menu
 clear 
 figlet 'KONTOL VPN' | lolcat
 echo -e "  ${z}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\033[0m${NC}"
-echo -e "  ${z}|\033[41m   ⇱ INFORMATIONS VPS By : LIGHTSTORE⇲       ${NC}${z}|${NC}"
+echo -e "  ${z}|\033[41m   ⇱ INFORMATIONS VPS By : LIGHTSTORE⇲      ${NC}${z}|${NC}"
 echo -e "  ${z}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\033[0m${NC}"
 echo -e "  ${z}|${r}⇲ ${NC}${z}SYSTEM OS     ${NC}: $MODEL"
 echo -e "  ${z}|${r}⇲ ${NC}${z}ISP           ${NC}: $ISP"
