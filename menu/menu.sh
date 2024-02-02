8a7bbb2d793713ff25808facdef06f7e2ee6aad5
Scvpsss <158008928+Scvpsss@users.noreply.github.com>
2024-02-02 19:35:01 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -260,9 +260,9 @@ read -n 1 -s -r -p "Press any key to back on menu"
 menu
 }
 clear 
-figlet 'KOREKEVPN' | lolcat
+figlet 'KONTOL VPN' | lolcat
 echo -e "  ${z}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\033[0m${NC}"
-echo -e "  ${z}|\033[41m   ⇱ INFORMATIONS VPS By : KOREKE⇲            ${NC}${z}|${NC}"
+echo -e "  ${z}|\033[41m   ⇱ INFORMATIONS VPS By : KOREKE⇲         ${NC}${z}|${NC}"
 echo -e "  ${z}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\033[0m${NC}"
 echo -e "  ${z}|${r}⇲ ${NC}${z}SYSTEM OS     ${NC}: $MODEL"
 echo -e "  ${z}|${r}⇲ ${NC}${z}ISP           ${NC}: $ISP"
