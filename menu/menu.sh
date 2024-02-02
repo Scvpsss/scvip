f22d0242b3d8f9754d3b48f776bb6e7f98c3c2c0
Scvpsss <158008928+Scvpsss@users.noreply.github.com>
2024-02-02 23:16:15 +0800

    Add files via upload

--- menu/menu.sh
+++ menu/menu.sh
@@ -285,7 +285,7 @@ echo -e "  ${z}│ [${r}03${NC}] ${z}VLESS  MENU  ${NC}       ""[${r}09${NC}] ${
 echo -e "  ${z}│ [${r}04${NC}] ${z}TROJAN MENU  ${NC}       ""[${r}10${NC}] ${z}INSTALL UDP        ${NC}"
 echo -e "  ${z}│ [${r}05${NC}] ${z}SETING MENU  ${NC}       ""[${r}11${NC}] ${z}ADD DOMAIN         ${NC}"        
 echo -e "  ${z}│ [${r}06${NC}] ${z}UPDATE SCRIPT${NC}       ""[${r}12${NC}] ${z}KELUAR SCRIPT      ${NC}"
-echo -e "  ${z}│ [${r}13${NC}] ${z}ADD IP VPS  ${NC}"
+echo -e "  ${z}│ [${r}13${NC}] ${z}ADD IP VPS  ${NC}        ""[${r}14${NC}] ${z}BOT MENU      ${NC}"
 echo -e "  ${z}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\033[0m${NC}"
 echo -e "${z}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\033[0m${NC}" 
 echo -e "${z}│${IWhite}HARI ini =${NC}${r} $ttoday${NC} ${IWhite}KEMARIN =${NC}${r} $tyest${NC} ${IWhite}BULAN =${NC}${r} $tmon${NC}"
@@ -312,4 +312,5 @@ case $opt in
 11) clear ; add-host ;; 
 12) exit ;;
 13) clear ; addip ;;
+14) clear ; bot ;;
 esac
